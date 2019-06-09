94712c73df233e96454ab40858be5db022fd0526
athulvingt <33945582+athulvingt@users.noreply.github.com>
2019-06-09 10:05:48 +0530

    Add files via upload

---
+++ java/com/example/tutorial/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tutorial;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
+        viewPager = (ViewPager) findViewById(R.id.main_pager);
+        ViewPagerAdapter adaptor = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adaptor.AddFragment(new TrackFragment(), "Tracks");
+
+        viewPager.setAdapter(adaptor);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void onClick(View v)
+    {
+
+    }
+}
